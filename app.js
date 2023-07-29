d8f7ee00f6384ef98e13c8fc5b59bb71a5b604ac
unknown <mitrovicbojan87@gmail.com>
2023-07-29 19:16:27 +0300

    obj

---
+++ app.js
@@ -0,0 +1,45 @@
+let myCruises = [
+  {
+    tahiti: {
+      A: {
+        price: 1000,
+        number: 10,
+      },
+      B: {
+        price: 800,
+        number: 5,
+      },
+      C: {
+        price: 300,
+        number: 0,
+      },
+    },
+  },
+  {
+    seychelles: {
+      A: {
+        price: 600,
+        number: 11,
+      },
+      B: {
+        price: 400,
+        number: 2,
+      },
+      C: {
+        price: 200,
+        number: 1,
+      },
+    },
+  },
+];
+
+function myFunction() {
+  let destination = document.getElementById("destination").value;
+  let category = document.getElementById("category").value;
+  let output = document.getElementById("output");
+  let outPrice = 0;
+  let avaCabin = 0;
+  for (let i of myCruises) {
+    console.log(i);
+  }
+}

b917ac0f33710f11c291670c54ca8149e7c866ba
unknown <mitrovicbojan87@gmail.com>
2023-08-21 21:30:48 +0300

    card

---
+++ payment.js
@@ -0,0 +1,7 @@
+// let creditCard = document.getElementById("credit-card").value;
+
+// $("credit-card").on("keypress change", function () {
+//   $(this).val(function (index, value) {
+//     return value.replace(/\W/gi, "").replace(/(.{4})/g, "$1 ");
+//   });
+// });
